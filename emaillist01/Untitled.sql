bc721def7154e772de8af13d6d94bf4db84352a8
dntjd7701 <dntjd7701@gmail.com>
2021-05-25 12:01:21 +0900

    emaillist01 #2

--- emaillist01/Untitled.sql
+++ emaillist01/Untitled.sql
@@ -3,6 +3,8 @@ use webdb;
 
 select no, first_name, last_name, email from emaillist01 order by no desc;
 
+delete from emaillist01 where no=?;
+
 insert
 	into emaillist01
 values(null,'둘','리', 'dooly@gmail.com')
