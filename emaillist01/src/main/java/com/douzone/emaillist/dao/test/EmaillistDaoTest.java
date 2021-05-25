9e5c93a59b41b45ed87fcf2e04c99d28e6aa1b1b
dntjd7701 <dntjd7701@gmail.com>
2021-05-25 10:57:01 +0900

    emaillist01 #1

---
+++ emaillist01/src/main/java/com/douzone/emaillist/dao/test/EmaillistDaoTest.java
@@ -0,0 +1,18 @@
+package com.douzone.emaillist.dao.test;
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+		insertTest();
+		findAllTest();
+	}
+
+	private static void insertTest() {
+		
+	}
+
+	private static void findAllTest() {
+		
+	}
+
+}
